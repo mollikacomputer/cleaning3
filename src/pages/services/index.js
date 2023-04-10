7c4b2edfe0cbe192d4532ced9a23b534e024a0b3
mollikacomputer <mollikacomputer3@gmail.com>
2023-04-10 16:26:40 +0600

    load service data into service page

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -1,10 +1,34 @@
-const index = () => {
-    return (
-        <div>
-            <h2 className="text-3xl"> Index of Service </h2>
-        </div>
-    );
+const index = ({ services }) => {
+  return (
+    <div className="container mx-10" >
+     <h2 className="card-title">Total Services:{services.length}</h2>
+        {
+            services.map(
+            service => 
+            <div className="card my-10  bg-base-100 shadow-xl">
+            <div className="card-body">
+            <h2 className="card-title">Total Services:{service.title}</h2>
+              <p>{service.description}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary"> read more... </button>
+              </div>
+            </div>
+          </div>
+          )
+        }
+      
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:3000/api/services");
+  const data = await res.json();
+  return {
+    props: {
+      services: data,
+    },
+  };
 };
 
 export default index;
-
