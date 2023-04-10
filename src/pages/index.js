8639ffc01b54efad4568ad32dc268f977ea2c641
mollikacomputer <mollikacomputer3@gmail.com>
2023-04-10 22:19:46 +0600

    update header image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import HeroSection from '../../components/header/HeroSection'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-     
+     <HeroSection/>
     <Link href={'/services'} >
     <button className="btn btn-success">service</button>
     </Link>
